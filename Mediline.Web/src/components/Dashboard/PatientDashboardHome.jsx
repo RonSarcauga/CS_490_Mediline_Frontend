6694dc9782f9c4ad828362c2088863825110f806
osid54 <osid.potato@gmail.com>
2025-04-07 21:51:47 -0400

    Added skeleton PatientDashboardHome component and scss

---
+++ Mediline.Web/src/components/Dashboard/PatientDashboardHome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../assets/scss/components/_patient-dashboard-home.scss';
+
+const PatientDashboard = () => {
+  return (
+    <div className="patient-dashboard">
+      {/* Top Horizontal Card */}
+      {/* Mid Horizontal Card */}
+      {/* Bottom Horizontal Card */}
+      {/* Right Vertical Card */}
+    </div>
+  );
+};
+
+export default PatientDashboard;
