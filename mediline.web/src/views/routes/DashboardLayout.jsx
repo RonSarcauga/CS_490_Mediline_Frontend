6076dcc9100ef1b294d9144f17980b0bd9a86334
RonSarcauga <rs55@njit.edu>
2025-04-15 08:25:49 -0400

    Added an import statement for Outlet in the DashboardLayout

--- mediline.web/src/views/routes/DashboardLayout.jsx
+++ mediline.web/src/views/routes/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Topbar, { TopbarItem } from '../../components/Dashboard/Topbar';
 import BaseIcon from '../../components/General/BaseIcon';
 import Container, { ItemGroup } from '../../components/General/Container';
