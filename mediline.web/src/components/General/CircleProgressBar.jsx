6bf6c716e341d6f880290cb3b9f1b3ff86a57266
RonSarcauga <rs55@njit.edu>
2025-04-27 08:06:32 -0400

    - Set up the basic structure and style of the "Circular Progress Bar"

--- mediline.web/src/components/General/CircleProgressBar.jsx
+++ mediline.web/src/components/General/CircleProgressBar.jsx
@@ -8,7 +8,8 @@ function CircleProgressBar({
 })
 {
     const radius = 85;
-
+    const dashArray = radius * Math.PI * 2;
+    const dashOffset = dashArray - (dashArray * percentage) / 100;
     return (
         <div>
             <svg
@@ -23,6 +24,22 @@ function CircleProgressBar({
                     r={radius}
                     className="circle-background"
                 />
+
+                <circle
+                    cx={circleWidth / 2}
+                    cy={circleWidth / 2}
+                    strokeWidth="15px"
+                    r={radius}
+                    className="circle-progress"
+                    style={{
+                        strokeDasharray: dashArray,
+                        strokeDashoffset: dashOffset,
+                    }}
+                    transform={`rotate(-90 ${circleWidth / 2} ${circleWidth / 2})`}
+                />
+                <text x="50%" y="50%" dy="0.3em" textAnchor="middle" className="circle-text">
+                    {percentage}% 
+                </text>
             </svg>
         </div>
     );
