00a206582db326108ac88427629eb6b7c58284f5
RonSarcauga <rs55@njit.edu>
2025-04-27 08:59:17 -0400

    - Changed some of the CSS rules for the Circular Progress Bar component - Adjusted the Progress Bar component to take more props to add more user customization - Adjusted the scaling of the Progress Bar component

--- mediline.web/src/components/General/CircleProgressBar.jsx
+++ mediline.web/src/components/General/CircleProgressBar.jsx
@@ -3,11 +3,17 @@
     It accepts percentage and circle width as a parameter.
 */ }
 function CircleProgressBar({
-    percentage,
-    circleWidth
+    fraction = 50,
+    total = 100,
+    circleWidth,
+    strokeColor,
+    progressColor,
+    text = `${fraction}`,
 })
 {
-    const radius = 85;
+    const percentage = (fraction / total) * 100;
+    const radius = circleWidth / 2 - 10;
+    const strokeWidth = Math.max(circleWidth * 0.08, 2);
     const dashArray = radius * Math.PI * 2;
     const dashOffset = dashArray - (dashArray * percentage) / 100;
     return (
@@ -20,8 +26,9 @@ function CircleProgressBar({
                 <circle
                     cx={circleWidth / 2}
                     cy={circleWidth / 2}
-                    strokeWidth="15px"
+                    strokeWidth={strokeWidth}
                     r={radius}
+                    stroke={strokeColor}
                     className="circle-background"
                 />
 
@@ -30,6 +37,8 @@ function CircleProgressBar({
                     cy={circleWidth / 2}
                     strokeWidth="15px"
                     r={radius}
+                    stroke={progressColor}
+                    strokeWidth={strokeWidth}
                     className="circle-progress"
                     style={{
                         strokeDasharray: dashArray,
@@ -37,8 +46,15 @@ function CircleProgressBar({
                     }}
                     transform={`rotate(-90 ${circleWidth / 2} ${circleWidth / 2})`}
                 />
-                <text x="50%" y="50%" dy="0.3em" textAnchor="middle" className="circle-text">
-                    {percentage}% 
+                <text
+                    x="50%"
+                    y="50%"
+                    dy="0.3em"
+                    textAnchor="middle"
+                    fontSize={Math.max(circleWidth * 0.15, 10)}
+                    className="circle-text"
+                >
+                    {text}
                 </text>
             </svg>
         </div>
--- mediline.web/src/views/routes/PHHome.jsx
+++ mediline.web/src/views/routes/PHHome.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import BaseIcon from '../../components/General/BaseIcon';
+import CircleProgressBar from '../../components/General/CircleProgressBar';
 import Container, { ItemGroup } from '../../components/General/Container';
 import { UserContext } from '../../context/UserProvider';
 import { dashboardLayoutViewModel } from '../../viewModels/DashboardLayoutViewModel';
@@ -10,6 +11,7 @@ function PHHome() {
     const user = dashboardLayoutViewModel.getUsers().find(user => user.id === currentUser.user.id);
     const pharmacistData = dashboardLayoutViewModel.getPharmacistData(user.id);
     const patients = dashboardLayoutViewModel.getCustomers(pharmacistData.pharmacyAddress);
+    const [percentage, setPercentage] = useState(35);
 
     return (
         <Container
@@ -92,29 +94,77 @@ function PHHome() {
                                                                         items={[
                                                                             <>
                                                                                 <Container
-                                                                                    customClass="gradient-white br-sm b-3 outline-neutral-1100 align-items-center justify-content-center"
+                                                                                    customClass="align-items-center justify-content-center"
                                                                                     fitParent={true}
                                                                                     content={[
                                                                                         <>
                                                                                             <ItemGroup
-                                                                                                customClass="gap-1 align-items-center justify-content-center pl-0 pr-4 py-4"
+                                                                                                customClass="gap-10"
                                                                                                 axis={false}
-                                                                                                stretch={true}
                                                                                                 items={[
                                                                                                     <>
-                                                                                                        <BaseIcon
-                                                                                                            fill="hsl(200, 30%, 25%)"
-                                                                                                            height="60px"
-                                                                                                            width="65px"
-                                                                                                            viewBox="0 0 100 100"
-                                                                                                            fillColor="hsl(200, 30%, 25%)"
-                                                                                                        >
-                                                                                                            <ellipse cx="41.3" cy="42.3" rx="12.2" ry="13.5" />
-                                                                                                            <path d="M52.6,57.4c-3.1,2.8-7,4.5-11.3,4.5c-4.3,0-8.3-1.7-11.3-4.6c-5.5,2.5-11,5.7-11,10.7v2.1 c0,2.5,2,4.5,4.5,4.5h35.7c2.5,0,4.5-2,4.5-4.5v-2.1C63.6,63,58.2,59.9,52.6,57.4z" />
-                                                                                                            <path d="M68,47.4c-0.2-0.1-0.3-0.2-0.5-0.3c-0.4-0.2-0.9-0.2-1.3,0.1c-2.1,1.3-4.6,2.1-7.2,2.1c-0.3,0-0.7,0-1,0 c-0.5,1.3-1,2.6-1.7,3.7c0.4,0.2,0.9,0.3,1.4,0.6c5.7,2.5,9.7,5.6,12.5,9.8H75c2.2,0,4-1.8,4-4v-1.9C79,52.6,73.3,49.6,68,47.4z" />
-                                                                                                            <path d="M66.9,34.2c0-4.9-3.6-8.9-7.9-8.9c-2.2,0-4.1,1-5.6,2.5c3.5,3.6,5.7,8.7,5.7,14.4c0,0.3,0,0.5,0,0.8 C63.4,43,66.9,39.1,66.9,34.2z" />
-                                                                                                        </BaseIcon>
-                                                                                                        <h4 className="font-semibold font-9 text-dark-200">{pharmacistData.patients.length}</h4>
+                                                                                                        <ItemGroup
+                                                                                                            customClass="gap-4"
+                                                                                                            axis={true}
+                                                                                                            items={[
+                                                                                                                <>
+                                                                                                                    <ItemGroup
+                                                                                                                        customClass="gap-1 align-items-center justify-content-center pl-0 pr-4 py-4"
+                                                                                                                        axis={false}
+                                                                                                                        stretch={true}
+                                                                                                                        items={[
+                                                                                                                            <>
+                                                                                                                                <CircleProgressBar
+                                                                                                                                    percentage={percentage}
+                                                                                                                                    circleWidth="100"
+                                                                                                                                    strokeColor="hsl(210, 35%, 50%)"
+                                                                                                                                    progressColor="hsl(200, 70%, 70%)"
+                                                                                                                                />
+                                                                                                                            </>
+                                                                                                                        ]}
+                                                                                                                    />
+                                                                                                                    <input
+                                                                                                                        type="range"
+                                                                                                                        min="0"
+                                                                                                                        max="100"
+                                                                                                                        step="1"
+                                                                                                                        value={percentage}
+                                                                                                                        onChange={(e) => setPercentage(e.target.value)}
+                                                                                                                    />
+                                                                                                                </>
+                                                                                                            ]}
+                                                                                                        />
+                                                                                                        <ItemGroup
+                                                                                                            customClass="gap-4"
+                                                                                                            axis={true}
+                                                                                                            items={[
+                                                                                                                <>
+                                                                                                                    <ItemGroup
+                                                                                                                        customClass="gap-1 align-items-center justify-content-center pl-0 pr-4 py-4"
+                                                                                                                        axis={false}
+                                                                                                                        stretch={true}
+                                                                                                                        items={[
+                                                                                                                            <>
+                                                                                                                                <CircleProgressBar
+                                                                                                                                    percentage={percentage}
+                                                                                                                                    circleWidth="200"
+                                                                                                                                    strokeColor="hsl(0, 0%, 40%)"
+                                                                                                                                    progressColor="hsl(45, 60%, 60%)"
+                                                                                                                                />
+                                                                                                                            </>
+                                                                                                                        ]}
+                                                                                                                    />
+                                                                                                                    <input
+                                                                                                                        type="range"
+                                                                                                                        min="0"
+                                                                                                                        max="100"
+                                                                                                                        step="1"
+                                                                                                                        value={percentage}
+                                                                                                                        onChange={(e) => setPercentage(e.target.value)}
+                                                                                                                    />
+                                                                                                                </>
+                                                                                                            ]}
+                                                                                                        />
                                                                                                     </>
                                                                                                 ]}
                                                                                             />
